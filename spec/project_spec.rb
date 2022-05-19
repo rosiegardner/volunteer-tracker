903ad1907c8a8e964d4c38f7e8f5fba23bf0c90d
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 15:00:08 -0700

    test id context for returning id of project after saving - expect fail

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -15,11 +15,11 @@ describe Project do
       expect(project.id).to eq nil
     end
 
-    # it 'returns the id of the project after saving project' do
-    #   project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-    #   project.save
-    #   expect(project.id).to be_an_instance_of Integer
-    # end
+    it 'returns the id of the project after saving project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      expect(project.id).to be_an_instance_of Integer
+    end
   end
 
   # describe '#==' do
