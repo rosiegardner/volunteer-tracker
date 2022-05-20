b8330872ee58865c0be15dc315543bc81c09021f
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 18:30:37 -0700

    add code for id attribute, .all, #save, #== methods - Passes TESTS

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,25 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id =>id}))
+    end
+    projects
+  end
+
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
+  end
+
+
 end
