8796df37c4beaaba4690af1cfcc6774d46a967e6
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 20:47:58 -0700

    add testing for update method - expect pass

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -55,4 +55,13 @@ describe Volunteer do
       expect(Volunteer.find(volunteer1.id)).to eq volunteer1
     end
   end
+
+  describe '#update' do
+    it 'updates a volunteer by id' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      volunteer1.update("Rosie", 1)
+      expect(volunteer1.name). to eq "Rosie"
+    end
+  end
 end
