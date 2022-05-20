354e960d40ec2a05db61b33336e21db7c5a5ffdf
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 19:40:35 -0700

    add 2nd test for .all, testing for #== and save method, expect passing

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -16,35 +16,35 @@ describe Volunteer do
     end
   end
 
-  # describe '#==' do
-  #   it 'checks for equality based on the name of a volunteer' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     expect(volunteer1 == volunteer2).to eq true
-  #   end
-  # end
+  describe '#==' do
+    it 'checks for equality based on the name of a volunteer' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      expect(volunteer1 == volunteer2).to eq true
+    end
+  end
 
   context '.all' do
     it 'is empty to start' do
       expect(Volunteer.all).to eq []
     end
 
-    # it 'returns all volunteers' do
-    #   volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-    #   volunteer1.save
-    #   volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
-    #   volunteer2.save
-    #   expect(Volunteer.all).to eq [volunteer1, volunteer2]
-    # end
+    it 'returns all volunteers' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2.save
+      expect(Volunteer.all).to eq [volunteer1, volunteer2]
+    end
   end
 
-  # describe '#save' do
-  #   it 'adds a volunteer to the database' do
-  #     volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-  #     volunteer1.save
-  #     expect(Volunteer.all).to eq [volunteer1]
-  #   end
-  # end
+  describe '#save' do
+    it 'adds a volunteer to the database' do
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1.save
+      expect(Volunteer.all).to eq [volunteer1]
+    end
+  end
 
   # describe '.find' do
   #   it 'returns a volunteer by id' do
