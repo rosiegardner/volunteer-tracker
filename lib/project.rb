13483d9efb78e5602c98a8afb069621ea7b208aa
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 18:52:04 -0700

    add code for delete method - PASSES TEST

--- lib/project.rb
+++ lib/project.rb
@@ -42,9 +42,9 @@ class Project
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
 
-  # def delete
-  #   DB.exec("DELETE FROM projects WHERE id = #{id};")
-  # end
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{id};")
+  end
 
 
 end
