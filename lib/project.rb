d6f4cb40a9fcf5346fff4c7bc1bebce203662bc6
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 18:50:30 -0700

    add code for update method testing - PASSES TEST

--- lib/project.rb
+++ lib/project.rb
@@ -37,5 +37,14 @@ class Project
     end
   end
 
+  def update(title)
+    @title = title
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
+
+  # def delete
+  #   DB.exec("DELETE FROM projects WHERE id = #{id};")
+  # end
+
 
 end
