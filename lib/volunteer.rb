a9da640c3b2fd790caab0f5431e50799458ca738
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-19 19:41:38 -0700

    update code for self.all, add code for #== and save method for testing- passes tests

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -8,7 +8,24 @@ class Volunteer
     @id = attributes.fetch(:id)
   end
 
+  def ==(volunteer_to_compare)
+    (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+  end
+
   def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
     volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
   end
 end
